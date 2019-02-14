0b20e4184bb57c8985c27f26fd963e1704c3d770
Thomas Harper <thomas.l.harper.ctr@navy.mil>
2019-02-14 15:14:00 -0800

    Clean up

--- src/main/java/com/example/utils/security/resource/autoconfig/ResourceServerConfig.java
+++ src/main/java/com/example/utils/security/resource/autoconfig/ResourceServerConfig.java
@@ -1,5 +1,7 @@
 package com.example.utils.security.resource.autoconfig;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.PriorityOrdered;
 import org.springframework.core.annotation.Order;
@@ -7,14 +9,26 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+
 
 @Configuration
 @EnableWebSecurity
 @Order(PriorityOrdered.HIGHEST_PRECEDENCE + 500)
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    String issuerUri;
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromOidcIssuerLocation(issuerUri);
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http
                 .csrf().disable()
                 .anonymous().disable()
